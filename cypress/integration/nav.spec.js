194695a7a14deb170c0733139cfe53b90e28e726
Satish <satishchilkaka8@gmail.com>
2020-12-25 23:40:01 -0500

    added config files

--- cypress/integration/nav.spec.js
+++ cypress/integration/nav.spec.js
@@ -1,15 +1,25 @@
 
-function loginUser(user, password) {
+let userName = Cypress.env('user')
+let password = Cypress.env('password')
+const ee = Cypress.env('user')
+
+describe('Navgation', () => {
+    it('it should navigate', () => {
+        loginUser(userName, password)
+    })
+
+    it('it should ', () => {
+        cy.visit('/admin')
+    })
+})
+
+function loginUser(userName, password) {
     cy.visit('as1-ui/login')
+    
     cy.get('.login-form').within(() => {
-        cy.get('input:first').type('satishc+fgdm0@autoserve1.com', { delay: 0, force: true });
-        cy.get('input:last').type('Rupert22', { delay: 0, force: true, log: false });
+        cy.get('input:first').type(userName, { delay: 0, force: true });
+        cy.get('input:last').type(password, { delay: 0, force: true, log: false });
     })
     cy.get('.btn-login').click().should('not.exist')
     cy.wait(2000)
 }
-describe('Navgation', () => {
-    it('it should navigate', () => {
-        loginUser()
-    })
-})
